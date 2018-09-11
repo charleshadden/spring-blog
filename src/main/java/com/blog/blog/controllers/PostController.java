3e29a1d60d1467166461c1649a560ee88e238c0c
charleshadden <charles.r.hadden@gmail.com>
2018-09-11 09:43:45 -0500

    adding to master

--- src/main/java/com/blog/blog/controllers/PostController.java
+++ src/main/java/com/blog/blog/controllers/PostController.java
@@ -1,51 +1,72 @@
 package com.blog.blog.controllers;
 
-import com.blog.blog.Post;
+import com.blog.blog.models.Post;
+import com.blog.blog.repositories.PostRepository;
 import com.blog.blog.services.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
 
     private final PostsService postSvc;
+    private PostRepository postDao;
 
-    public PostController(PostsService postSvc) {
+    public PostController(PostsService postSvc, PostRepository postDao) {
+
         this.postSvc = postSvc;
+        this.postDao = postDao;
     }
 
-    @GetMapping("/posts/{id}")
-    public String showPost(@PathVariable long id, Model vModel) {
-        Post post = postSvc.findOne(id);
-        vModel.addAttribute("post", post);
+    @GetMapping("/posts/index")
+    public String FindAll(Model vModel) {
+
+        vModel.addAttribute("posts", postDao.findAll());
+
+//        List<Post> posts = postSvc.findAllPosts();
+//        vModel.addAttribute("posts", posts);
+        return"posts/index";
+    }
+
+    @RequestMapping("/posts/{id}")
+    public String viewAd(@PathVariable("id") Long id, Model model) {
+//        Post post = postSvc.findById(id);
+        model.addAttribute("post", postDao.findById(id));
         return "posts/show";
     }
 
-    @GetMapping(path="/posts")
-    private String showAllPosts(Model vModel) {
-        vModel.addAttribute("posts", postSvc.findAll());
-        return "posts/index";
+    @RequestMapping("/posts/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        postSvc.deletePost(id);
+                return "redirect:/posts/index";
     }
 
-    @GetMapping(path="/posts/create")
-    public String showCreateForm(Model vModel) {
+    @GetMapping("/posts/create")
+    public String showCreateForm(Model vModel){
         vModel.addAttribute("post", new Post());
         return "posts/create";
     }
 
-
-    @PostMapping("/posts/save")
-    public String createAd(@ModelAttribute Post post){
-        postSvc.save(post);
-        return "redirect:/posts/";
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post){
+        postSvc.savePost(post);
+        return "redirect:/posts/index";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(Model vModel, @PathVariable long id) {
+        Post existingPost = postSvc.findById(id);
+        vModel.addAttribute("post", existingPost);
+        return "posts/edit";
+    }
 
-
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @ModelAttribute Post post) {
+        post.setId(id);
+        postSvc.savePost(post);
+        return "redirect:/posts/index";
+    }
 
 
 }
--- src/main/java/com/blog/blog/services/PostsService.java
+++ src/main/java/com/blog/blog/services/PostsService.java
@@ -1,39 +1,39 @@
 package com.blog.blog.services;
 
 
-import com.blog.blog.Post;
+import com.blog.blog.models.Post;
+import com.blog.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostsService {
 
-    private List<Post> posts;
+    @Autowired
+    private final PostRepository postDao;
 
-    public PostsService() {
-        posts = new ArrayList<>();
-        createPosts();
+    public PostsService(PostRepository postDao) {
+        this.postDao = postDao;
     }
 
-    public Post save(Post post) {
-        post.setId(posts.size() + 1);
-        posts.add(post);
-        return post;
+
+    public List<Post> findAllPosts() {
+        return postDao.findAll();
     }
 
-    public Post findOne(long id) {
-        return posts.get((int)id - 1);
+    public Post findById(Long id) {
+        return postDao.findOne(id);
     }
 
-    public List<Post> findAll() {
-        return posts;
+    public void deletePost(Long id) {
+        postDao.delete(id);
     }
 
-    private void createPosts() {
-        posts.add(new Post("Check out this movie","Sicario 2 is a really good movie and you should go see it."));
-        posts.add(new Post("Nike","The new ad is horrible and I can't believe a company can get away with that."));
-        posts.add(new Post("2020","I wonder who will be elected?"));
+    public Post savePost(Post posts) {
+        return postDao.save(posts);
     }
+
+
 }
